26c4525c9b37d1b6fe67c0492083dd822dafa51b
vanessa <vanessa_tommo94@outlook.com>
2018-06-18 10:22:41 +0200

    aggiunto prodotti

--- js/cupcakes.js
+++ js/cupcakes.js
@@ -1,11 +1,48 @@
 var cupcakes = [{
     id: 1,
     nome: 'Cupcake Flamingo',
-    foto: " ",
-    descrizione:" ",
-    ingredienti: ['', '', ''],
-    prezzo: ""
+    foto: "../img/flamingo.jpg ",
+    descrizione:"are a type of wading bird in the family Phoenicopteridae, the only bird family in the order Phoenicopteriformes. Four flamingo species are in the Americas and two species are in the Old World. ",
+    ingredienti: ['self-raising flour', 'butter', 'caster sugar', 'sugar', 'vanilla'],
+    prezzo: 5.
 }, {
+    id:2,
+    nome: 'Cupcake Mermaid',
+    foto: "../img/mermaind.jpg",
+    descrizione: "Mermaid Cupcakes sono splendidi cupcakes ispirati all'oceano e alla magia delle sirene! I cupcakes tropicali al cocco sono decorati con "sabbia" di cocco tostata, code di sirena commestibili, perle di zucchero e conchiglie di cioccolato luccicante",
+    ingredienti:  ['self-rasing flour','butter', 'caster sugar', 'eggs', 'vanilla', 'blue coloring'],
+    prezzo: 6.
 
-},
+}, {
+        id: 3,
+        nome: 'Cupcake Unicorn',
+        foto: "../img/unicorn.jpg",
+        descrizione: "Cupcakes color pastello, candidi gonfiabili che invadono le spiagge, accessori all’ultima moda su cui spuntano inaspettati arcobaleni colorati",
+        ingredienti: ['self-rasing flour', 'butter', 'caster sugar', 'eggs', 'vanilla'],
+        prezzo: 7.
+
+},{
+        id: 4,
+        nome: 'Cupcake Red Velvet',
+        foto: "../img/redvelvet.jpg",
+        descrizione: "Cosa c’è di più romantico e goloso se non un Red Velvet Cupcake? Un soffice cupcake rosso come la passione e ricoperto da un frosting golosissimo a cui nessuno può resistere",
+        ingredienti: ['self-rasing flour', 'butter', 'caster sugar', 'eggs', 'vanilla', 'red coloring'],
+        prezzo: 4.
+
+},{
+
+        id: 5,
+        nome: 'Cupcake fragole & chocolate',
+        foto: "../img/fragole.jpg",
+        descrizione: "I cupcake al cioccolato con crema al cioccolato e fragole sono dei dolcetti davvero deliziosi, golosi e con un tocco di freschezza dato dalla frutta",
+        ingredienti: ['self-rasing flour', 'butter', 'caster sugar', 'eggs', 'vanilla', 'strawberry'],
+        prezzo: 4.
+},{
+        id: 6,
+        nome: 'Cupcake chocolate',
+        foto: "../img/chocolate.jpg",
+        descrizione: "I cupcake al cioccolato sono morbidi e golosissimi tortini, arricchiti da tanto cioccolato perfetto per i piu golosi",
+        ingredienti: ['self-rasing flour', 'butter', 'caster sugar', 'eggs', 'vanilla', 'chocolate'],
+        prezzo: 3.50
+}
 ];
